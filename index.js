0f34ee84c48b653c5e33d606f95b2fb6d7c749e6
ills5771 <ilyayelizarov1@gmail.com>
2019-03-18 12:42:26 -0700

    initial setup, first GET endpoint implemented

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+server.get("/users", (req, res) => {
+  const hobbits = [
+    {
+      name: "Jane Doe",
+      bio: "Not Tarzan's Wife, another Jane"
+    }
+  ];
+
+  res.status(200).json(hobbits);
+});
+
+server.listen(8000, () => console.log("API running on port 8000"));
