1000b42de9bbd87ad8315adcfcd2b81272c56ddd
ills5771 <ilyayelizarov1@gmail.com>
2019-03-18 13:47:51 -0700

    changed GET request for /users and added GET for users/:id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // implement your API here
 
 const express = require("express");
+const db = require("./data/db.js");
 
 const server = express();
 
@@ -9,14 +10,28 @@ server.get("/", (req, res) => {
 });
 
 server.get("/users", (req, res) => {
-  const hobbits = [
-    {
-      name: "Jane Doe",
-      bio: "Not Tarzan's Wife, another Jane"
-    }
-  ];
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "The users information could not be retrieved." });
+    });
+});
 
-  res.status(200).json(hobbits);
+server.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "The user information could not be retrieved." });
+    });
 });
 
 server.listen(8000, () => console.log("API running on port 8000"));
